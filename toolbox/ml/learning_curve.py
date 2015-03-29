5ec4ffb01dea5d83a7ecaf2525726e5b719c8156
amccabe <alix.mccabe@students.olin.edu>
2015-03-28 22:45:54 -0400

    Adding Machine Learning Toolbox! Yay!

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -7,20 +7,38 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
-print data.DESCR
 num_trials = 10
 train_percentages = range(5,95,5)
-test_accuracies = numpy.zeros(len(train_percentages))
 
 # train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
 # the resultant accuracy.
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
+def get_plots(data):
+	
+	averages = []
+
+	for i in range(5,95,5):
+		counter = 0
+		test = []
+
+		while counter <= 10:
+			counter += 1
+
+			test_accuracies = numpy.zeros(len(train_percentages))
+			X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=i*0.01)
+			model = LogisticRegression(C=10**-10)
+			model.fit(X_train, y_train)
+
+			test.append(float("%f" %model.score(X_test,y_test)))
+
+		averages.append(sum(test)/len(test))
+
+	return averages
 
 fig = plt.figure()
-plt.plot(train_percentages, test_accuracies)
+plt.plot(range(5,95,5),get_plots(data))
 plt.xlabel('Percentage of Data Used for Training')
 plt.ylabel('Accuracy on Test Set')
 plt.show()
